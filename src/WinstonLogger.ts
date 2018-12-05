0c7b230b9af5bedeb030baeef1160dab9cd3e9bf
guy120494@gmail.com <yarinvak@gmail.com>
2018-12-05 14:27:34 +0200

    adding colors and pretty json

--- src/WinstonLogger.ts
+++ src/WinstonLogger.ts
@@ -1,21 +1,41 @@
 import * as winston from 'winston';
+import {ParserUtil} from "./utils/ParserUtil";
 
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, colorize, printf, splat } = format;
 export class WinstonLogger {
     private logger: winston.Logger;
 
     constructor() {
+        const myFormat =  winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.align(),
+            winston.format.printf((info) => {
+                const {
+                    timestamp, level, message, ...args
+                } = info;
 
-        this.logger = winston.createLogger({
-            level: 'debug',
-            format: winston.format.json(),
+                const ts = timestamp.slice(0, 19).replace('T', ' ');
+                return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+            }));
+
+        this.logger = createLogger({
+            level: 'warn',
+            format: format.json(),
             transports: [
-                new winston.transports.Console(),
-                new winston.transports.File({filename: 'somefile.log'})
+                new transports.File({filename: 'somefile.log',format: combine(
+                        myFormat
+                    )}, )
             ],
             exitOnError: false, // do not exit on handled exceptions
         });
-
+        this.logger.add(new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                myFormat)
+        }));
     }
+
 
     getLogger() {
         return this.logger;
