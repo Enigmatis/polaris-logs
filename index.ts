8b043ea69554c6d6a7f7d044cd4ad5ba5d9c2971
chenshoo <chenshooker@gmail.com>
2019-01-10 15:45:05 +0200

    working

--- index.ts
+++ index.ts
@@ -2,9 +2,12 @@ import {PolarisLogProperties} from "./src/PolarisLogProperties"
 import {PolarisLogger} from "./src/PolarisLogger"
 import {ApplicationLogProperties} from "./src/entities/ApplicationLogProperties"
 import {LogPropertiesWrapper} from "./src/LogPropertiesWrapper"
+import {LoggerConfiguration} from "./src/LoggerConfiguration";
+
 export {
     PolarisLogger,
     PolarisLogProperties,
     ApplicationLogProperties,
-    LogPropertiesWrapper
+    LogPropertiesWrapper,
+    LoggerConfiguration
 };
