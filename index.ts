0eddf62dc12eed3a629a8556afd40c9117b5ab15
bikov <bikov95@gmail.com>
2019-01-14 11:28:21 +0200

    export logger configuration

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { ApplicationLogProperties } from './src/entities/application-log-properties';
 export { PolarisLogProperties } from './src/polaris-log-properties';
 export { PolarisLogger } from './src/polaris-logger';
+export { LoggerConfiguration } from './src/logger-configuration';
