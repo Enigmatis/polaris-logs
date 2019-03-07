a79fd3f627ddf4626524f49b41bf8b5894c513d8
osher-sade <osher.sade@gmail.com>
2019-03-07 15:41:42 +0200

    feat: added support for multiple logstash services

--- src/winston-logger.ts
+++ src/winston-logger.ts
@@ -9,7 +9,7 @@ const consoleFullFormat = winston.format.combine(
     winston.format.printf(info => {
         const { timestamp, level, message, ...args } = info;
         const ts = timestamp.slice(0, 19).replace('T', ' ');
-        return `${ts} [${level}]: ${message}${
+        return `${ts} [${level}]: ${message} ${
             Object.keys(args).length ? `\n${JSON.stringify(args, null, 2)}` : ''
         }`;
     }),
