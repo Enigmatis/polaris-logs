b49cb73b1f0cb57b77442ff2149aff22759caa9f
DoctorVoid <osher.sade@gmail.com>
2020-02-20 16:24:12 +0200

    fix: abstract logger changes

--- src/polaris-logger.ts
+++ src/polaris-logger.ts
@@ -1,34 +1,14 @@
 import { ApplicationProperties } from '@enigmatis/polaris-common';
-import cleanDeep = require('clean-deep');
-import * as serializeError from 'serialize-error';
-import { Logger } from 'winston';
 import { AbstractPolarisLogger } from './abstract-polaris-logger';
 import { LoggerConfiguration } from './configurations/logger-configuration';
 import { PolarisLogProperties } from './polaris-log-properties';
-import { createLogger } from './winston-logger';
-const uuidv1 = require('uuid/v1');
 
 export class PolarisLogger extends AbstractPolarisLogger {
-    private static getAppPropertiesToAssign(applicationProperties?: ApplicationProperties) {
-        if (applicationProperties) {
-            return {
-                systemId: applicationProperties.id,
-                systemName: applicationProperties.name,
-                version: applicationProperties.version,
-                environment: applicationProperties.environment,
-                component: applicationProperties.component,
-            };
-        }
-    }
-
-    private logger: Logger;
-
     public constructor(
         loggerConfiguration: LoggerConfiguration,
-        readonly applicationLogProperties?: ApplicationProperties,
+        applicationLogProperties?: ApplicationProperties,
     ) {
-        super();
-        this.logger = createLogger(loggerConfiguration);
+        super(loggerConfiguration, applicationLogProperties);
     }
 
     public fatal(message: string, polarisLogProperties?: PolarisLogProperties) {
@@ -53,29 +33,5 @@ export class PolarisLogger extends AbstractPolarisLogger {
 
     public trace(message: string, polarisLogProperties?: PolarisLogProperties) {
         this.logger.trace(this.buildLog(message, polarisLogProperties));
-    }
-
-    private buildLog(message: string, polarisLogProperties?: PolarisLogProperties) {
-        const eventKindDescription =
-            this.applicationLogProperties?.id || polarisLogProperties?.request?.requestingSystem?.id
-                ? {
-                      systemId: this.applicationLogProperties?.id,
-                      requestingSystemId: polarisLogProperties?.request?.requestingSystem?.id,
-                  }
-                : undefined;
-        const messageId = polarisLogProperties?.messageId || uuidv1();
-        const propertiesWithCustom = {
-            message,
-            ...(polarisLogProperties && polarisLogProperties.customProperties),
-            ...polarisLogProperties,
-            ...PolarisLogger.getAppPropertiesToAssign(this.applicationLogProperties),
-            eventKindDescription,
-            messageId,
-            throwable:
-                polarisLogProperties &&
-                polarisLogProperties.throwable &&
-                serializeError(polarisLogProperties.throwable),
-        };
-        return cleanDeep(propertiesWithCustom);
     }
 }
