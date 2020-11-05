1e4e40948bc26fd8510475ff06c29dea3b565500
itaykl <itaykl123@gmail.com>
2020-11-05 12:10:10 +0200

    refactor: another cr fixes

--- src/entities/group-id.ts
+++ src/entities/group-id.ts
@@ -1,8 +1,6 @@
-import { KeyValuePair } from './key-value-pair';
-
 export interface GroupId {
     id: string;
     name?: string;
-    secondaryIds?: KeyValuePair<string, any>[];
+    secondaryIds?: Record<string, any>[];
     action: string;
 }
--- src/entities/sub-entity.ts
+++ src/entities/sub-entity.ts
@@ -1,11 +1,10 @@
 import { CentralPoint } from './central-point';
-import { KeyValuePair } from './key-value-pair';
 
 export interface SubEntity {
     id: string;
     name?: string;
-    secondaryIds?: KeyValuePair<string, any>[];
-    operationalData?: KeyValuePair<string, any>[];
-    criticalFields?: KeyValuePair<string, any>[];
+    secondaryIds?: Record<string, any>[];
+    operationalData?: Record<string, any>[];
+    criticalFields?: Record<string, any>[];
     centralPoint?: CentralPoint;
 }
