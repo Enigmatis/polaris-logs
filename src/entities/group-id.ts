3d7a5f48582d54f6613a0aa1e2e13b984d7660f8
itaykl <43351063+itaykl@users.noreply.github.com>
2020-11-05 11:21:07 +0200

    Revert "BREAKING CHANGE: updated and added new log properties (#81)"
    This reverts commit e2671e653bc0f6c8132222b423efd4816ad41935.

--- src/entities/group-id.ts
+++
@@ -1,8 +0,0 @@
-import { KeyValuePair } from './key-value-pair';
-
-export interface GroupId {
-    id: string;
-    name?: string;
-    secondaryIds?: KeyValuePair<string, any>[];
-    action: string;
-}
