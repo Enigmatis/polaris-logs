49f67d83b7337f690cb108b6d364c2e59434c573
itaykl <itaykl123@gmail.com>
2020-11-05 10:45:34 +0200

    BREAKING CHANGE: updated and added new log properties

--- src/entities/sub-entity.ts
+++ src/entities/sub-entity.ts
@@ -1,18 +1,11 @@
 import { CentralPoint } from './central-point';
-import { Identifiable } from './identifiable';
 import { KeyValuePair } from './key-value-pair';
-import { Reality } from './reality';
-import { SubEntity } from './sub-entity';
 
-export interface Entity {
-    id?: string;
-    reality?: Reality;
+export interface SubEntity {
+    id: string;
     name?: string;
     secondaryIds?: KeyValuePair<string, any>[];
     operationalData?: KeyValuePair<string, any>[];
-    correlationIds?: Identifiable[];
-    subEntities?: SubEntity[];
     criticalFields?: KeyValuePair<string, any>[];
     centralPoint?: CentralPoint;
-    isClassified?: boolean;
 }
